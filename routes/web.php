df0a0dc750050a8310d096120b1722a4fc409fa5
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-08-05 22:57:23 +0430

    product Route

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// this code interact with product controller and in here ProductController is
+// the name of the class, index is the action or method which will implement
+// and we give the route a name which is product.index and this option is normal.
 Route::get('/', [ProductController::class, 'index'])->name('product.index');
