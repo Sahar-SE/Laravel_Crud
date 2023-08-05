34fe151822d5546e84d67df7f099f3cd7643be2f
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-08-05 22:52:28 +0430

    product Route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ProductController::class, 'index'])->name('product.index');
