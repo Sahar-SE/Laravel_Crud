4e566167e6f547e79f275803d5427346416f0947
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-08-06 19:14:16 +0430

    create a navbar

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,9 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Products</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
+<div class="bg-gray-800 p-2">
+  <nav class="flex justify-between">
+    <div>
+      <a href="#" class="text-white text-lg font-semibold px-2">Products</a>
+    </div>
+    <div class="space-x-4">
+      <a href="#" class="text-gray-300 hover:text-white">Home</a>
+      <a href="#" class="text-gray-300 hover:text-white">About</a>
+      <a href="#" class="text-gray-300 hover:text-white">Services</a>
+      <a href="#" class="text-gray-300 hover:text-white">Contact</a>
+    </div>
+    <div>
+      <input type="text" placeholder="Search" class="px-2 py-1 rounded-md">
+    </div>
+  </nav>
+</div>
+
     <h1>Products</h1>
     
 </body>
