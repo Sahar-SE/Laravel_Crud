2fe2832a7ce91154a4ccd6cf823e3192f6e113b1
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-08-06 20:28:11 +0430

    create a navbar

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -6,6 +6,22 @@
     <title>Document</title>
 </head>
 <body>
+<div class="bg-gray-800 p-2">
+  <nav class="flex justify-between">
+    <div>
+      <a href="/" class="text-white text-lg font-semibold px-2">Products</a>
+    </div>
+    <div class="space-x-4">
+      <a href="#" class="text-gray-300 hover:text-white">Home</a>
+      <a href="#" class="text-gray-300 hover:text-white">About</a>
+      <a href="#" class="text-gray-300 hover:text-white">Services</a>
+      <a href="#" class="text-gray-300 hover:text-white">Contact</a>
+    </div>
+    <div>
+      <input type="text" placeholder="Search" class="px-2 py-1 rounded-md">
+    </div>
+  </nav>
+</div>
     <h1>Create New Product</h1>
 </body>
 </html>
