7680797f34553ac303ba45b603b5bade02bc145e
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-08-06 22:29:53 +0430

    Update Controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -17,5 +17,7 @@ class ProductController extends Controller
   public function store(Request $request){
     $imageName = time().'.'.$request->image->extension();
     $request->image->move(public_path('products'), $imageName);
+
+    dd($imageName);
   }
 }
