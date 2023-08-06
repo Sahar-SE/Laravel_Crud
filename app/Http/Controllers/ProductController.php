975db7c800e8c30f5ce10990d4b97981ab8a07aa
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-08-06 16:09:11 +0430

    product create action

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,4 +9,8 @@ class ProductController extends Controller
   public function index(){
     return view('product.index');
   }
+
+  public function create(){
+    return view('product.create');
+  }
 }
