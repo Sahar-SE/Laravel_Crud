678409661128110c4b668ddf7100d489ba59b96a
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-08-07 21:41:41 +0430

    Update Routes

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Route::get('/', [ProductController::class, 'index'])->name('products.index');
 Route::get('products/create', [ProductController::class, 'create'])->name('products.create');
 Route::post('products/store', [ProductController::class, 'store'])->name('products.store');
 Route::get('products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
+Route::post('products/{id}/update', [ProductController::class, 'update'])->name('products.update');
