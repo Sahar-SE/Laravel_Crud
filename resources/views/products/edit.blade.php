d42ccc96c8f9eae65bb81e2cdbbb3f046cfd335d
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-08-07 21:30:24 +0430

    Update edit view

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -36,7 +36,7 @@
       <div class="mb-4">
           <label class="block text-gray-700 text-sm font-bold mb-2">Name</label>
           <input type="text"
-          name="name" value="{{ old('name') }}"
+          name="name" value="{{ old('name', $product->name) }}"
           class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700
            leading-tight focus:outline-none focus:shadow-outline"/>
 
