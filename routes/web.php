651b7f1521c99a618aafc738bb0c78e93c63e8e4
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-08-07 20:51:17 +0430

    Update Routes

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ use App\Http\Controllers\ProductController;
 Route::get('/', [ProductController::class, 'index'])->name('products.index');
 Route::get('products/create', [ProductController::class, 'create'])->name('products.create');
 Route::post('products/store', [ProductController::class, 'store'])->name('products.store');
+Route::get('products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
