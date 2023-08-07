12f76e41e0fab2b929641f00c238704b0315cf85
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-08-07 20:40:22 +0430

    Update index view

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -29,8 +29,8 @@
   <table class="min-w-max w-full table-auto">
     <thead>
       <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+      <th class="py-3 px-6 text-left">S.No</th>
         <th class="py-3 px-6 text-left">Name</th>
-        <th class="py-3 px-6 text-left">Description</th>
         <th class="py-3 px-6 text-left">Image</th>
         <th class="py-3 px-6 text-left w-1/2">Actions</th>
       </tr>
@@ -38,8 +38,8 @@
     <tbody class="text-gray-600 text-sm font-light">
       @foreach($products as $product)
         <tr class="border-b border-gray-200 hover:bg-gray-100">
+        <td class="py-3 px-6 text-left">{{ $loop->index }}</td>
           <td class="py-3 px-6 text-left whitespace-nowrap">{{ $product->name }}</td>
-          <td class="py-3 px-6 text-left">{{ $product->description }}</td>
           <td class="py-3 px-6 text-left"><img src="{{ asset('public/products/'.$product->image) }}" alt="" width="100"></td>
           <td class="py-3 px-6 text-left flex">
             <a href="products/{{ $product->id }}/edit" class="p-3 bg-blue-500 text-white rounded-md m-2">Edit</a>
