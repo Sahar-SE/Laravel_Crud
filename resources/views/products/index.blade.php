0332b022914b85f0537adf1738c009517cdb110b
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-08-14 19:01:18 +0430

    Comment for readablity

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -24,6 +24,7 @@
       </tr>
     </thead>
     <tbody class="text-gray-600 text-sm font-light">
+      <!-- Create a foreach to loop through data in database -->
       @foreach($products as $product)
         <tr class="border-b border-gray-200 hover:bg-gray-100">
         <td class="py-3 px-6 text-left">{{ $loop->index+1 }}</td>
