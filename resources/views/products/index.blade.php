44d62c649ba4e2f6a75b5a8487017cf754090f6e
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-08-14 19:03:59 +0430

    Comment for readablity

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -34,6 +34,8 @@
             <img src="products/{{ $product->image }}" class="rounded-full w-16 h-16"></td>
           <td class="py-3 px-6 text-left flex">
             <a href="products/{{ $product->id }}/edit" class="p-3 bg-blue-500 text-white rounded-md m-2">Edit</a>
+
+            // Create a form for the delete of a product and it will redirected to action path
             <form action="products/{{ $product->id }}/delete" method="POST">
               @csrf
               @method('DELETE')
